38a03cf5d7d71eeef2e73858a4b746be4ccaf3cb
tangzhihui <finetang@gmail.com>
2013-01-17 00:01:14 +0800

    增加注释

--- get_cnBeta.py
+++ get_cnBeta.py
@@ -4,21 +4,24 @@ import lxml.html
 import urllib2
 import lxml.etree
 
+# 获取首页所有链接，返回一个list，其包含编号＼文章链接地址＼文章标题
 #url='http://www.cnbeta.com'
-
 def getList(addr):
+    # 读入页面
     dom = lxml.html.fromstring(urllib2.urlopen(addr).read())
+    # 设计获取条件
     tre = dom.xpath('//dt[@class="topic"]/a')
     res = len(tre)
     result = []
+    # 组合返回内容
     for index in range(res):
     	result.append([str(index + 1), addr + tre[index].get('href'), tre[index].getchildren()[0].text])
     return result
 
 #print getList(url)[0][2]
 
+# 获取指定链接地址的文章内容，返回一个list，其为纯文本，不包含格式
 #con='http://www.cnbeta.com/articles/222586.htm'
-
 def getContent(addr):
     dom = lxml.html.fromstring(urllib2.urlopen(addr).read())
     title = dom.xpath('//h3[@id="news_title"]')
