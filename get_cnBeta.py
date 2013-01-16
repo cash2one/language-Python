7e2fca73a5b7fe4eadaf5f07fc1a5288859c9e65
tangzhihui <finetang@gmail.com>
2013-01-16 23:56:04 +0800

    修改以支持获取所有链接和文章内容

--- get_cnBeta.py
+++ get_cnBeta.py
@@ -4,12 +4,32 @@ import lxml.html
 import urllib2
 import lxml.etree
 
+#url='http://www.cnbeta.com'
+
+def getList(addr):
+    dom = lxml.html.fromstring(urllib2.urlopen(addr).read())
+    tre = dom.xpath('//dt[@class="topic"]/a')
+    res = len(tre)
+    result = []
+    for index in range(res):
+    	result.append([str(index + 1), addr + tre[index].get('href'), tre[index].getchildren()[0].text])
+    return result
+
+#print getList(url)[0][2]
+
+#con='http://www.cnbeta.com/articles/222586.htm'
+
+def getContent(addr):
+    dom = lxml.html.fromstring(urllib2.urlopen(addr).read())
+    title = dom.xpath('//h3[@id="news_title"]')
+    content = dom.xpath('//div[@id="news_content"]')
+    return [title[0].text, content[0].text_content()]
+
+#res = getContent(con)
+#print res[0] + '\n' + res[1]
+
 url='http://www.cnbeta.com'
-
-dom = lxml.html.fromstring(urllib2.urlopen(url).read())
-tre = dom.xpath('//dt[@class="topic"]/a')
-res = len(tre)
-
-for index in range(res):
-	print index + 1, url + tre[index].get('href'), tre[index].getchildren()[0].text
-
+l = getList(url)
+for item in l:
+    res = getContent(item[1])
+    print res[0] + '\n' + res[1]
