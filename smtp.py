93b0ab7cd199abd1c2f32eed5581cf3223bebfd8
tangzhihui <finetang@gmail.com>
2013-01-29 22:35:23 +0800

    automatic send an email with smtplib

---
+++ smtp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import smtplib
+from email.mime.text import MIMEText
+
+mail_list=['majordomo@vger.kernel.org']
+mail_host='smtp.exmail.qq.com'
+mail_user=''
+mail_pass=''
+mail_prefix=''
+
+def send_mail(send_to, subject, body):
+    me = mail_user + '<' + mail_user + '@' + mail_prefix + '>'
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = ';'.join(send_to)
+
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)
+        s.login(mail_user, mail_pass)
+        s.sendmail(me, send_to, msg.as_string())
+        s.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+
+if __name__ == '__main__':
+    if send_mail(mail_list, '', 'subscribe linux-kernel'):
+        print 'OK!'
+    else:
+        print 'Failed!'
+
