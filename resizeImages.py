354778522ed2f2d69ce86a25c70ffa0bbb4f4e6f
Tom <finetang@gmail.com>
2013-06-25 21:56:30 +0800

    Update resizeImages.py

--- resizeImages.py
+++ resizeImages.py
@@ -5,11 +5,12 @@ MAX_SIZE = 2048
 
 def resize(imageName):
 	img = Image.open(imageName)
-	width = MAX_SIZE * img.size[0] / img.size[1] if img.size[0] <= img.size[1] else MAX_SIZE if img.size[0] > MAX_SIZE else img.size[0]
-	height = MAX_SIZE * img.size[1] / img.size[0] if img.size[0] >= img.size[1] else MAX_SIZE if img.size[1] > MAX_SIZE else img.size[1]
+	width = img.size[0] if img.size[0] <= MAX_SIZE and img.size[1] <= MAX_SIZE else MAX_SIZE if img.size[0] > img.size[1] else MAX_SIZE * img.size[0] / img.size[1]
+	height = img.size[1] if img.size[0] <= MAX_SIZE and img.size[1] <= MAX_SIZE else MAX_SIZE * img.size[1] / img.size[0] if img.size[0] >= img.size[1] else MAX_SIZE
+	print 'width:', width, 'height:', height,
 	resized_img = img.resize((width, height), Image.ANTIALIAS)
 	basename, extension = os.path.splitext(imageName)
-	resized_img.save(basename + '_resized.jpg')
+	resized_img.save(basename + '_new.jpg')
 
 if __name__ == '__main__':
 	imgList = os.listdir(os.path.abspath(os.curdir))
